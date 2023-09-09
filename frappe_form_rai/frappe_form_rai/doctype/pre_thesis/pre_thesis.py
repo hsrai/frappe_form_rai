92528e42c0230bbe497a6c384ecc6f64ffff51d0
HSRaiExp <hardeep.rai@gmail.com>
2023-09-09 05:52:15 +0530

    sync

--- frappe_form_rai/frappe_form_rai/doctype/pre_thesis/pre_thesis.py
+++ frappe_form_rai/frappe_form_rai/doctype/pre_thesis/pre_thesis.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2023, H S Rai and contributors
 # For license information, please see license.txt
 
-# import frappe
+#import frappe
 from frappe.model.document import Document
 
 class PreThesis(Document):
+	#full_name = get_user_fullname()
 	pass
--- frappe_form_rai/frappe_form_rai/web_form/daily_progress_pt/daily_progress_pt.py
+++ frappe_form_rai/frappe_form_rai/web_form/daily_progress_pt/daily_progress_pt.py
@@ -2,4 +2,4 @@ import frappe
 
 def get_context(context):
 	# do your magic here
-	pass
+	pass	
