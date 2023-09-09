6d79165fed125972b711e78152243df0cac9cff3
HSRaiExp <hardeep.rai@gmail.com>
2023-09-09 11:45:48 +0530

    diameter changed from data to float, area/perimetr calculated

--- frappe_form_rai/frappe_form_rai/doctype/circle/circle.py
+++ frappe_form_rai/frappe_form_rai/doctype/circle/circle.py
@@ -3,6 +3,10 @@
 
 # import frappe
 from frappe.model.document import Document
+import math as math
 
 class Circle(Document):
-	pass
+    #this method will run every time a document is saved
+    def before_save(self):
+    	self.area = round(math.pi * self.diameter ** 2 / 4., 3)
+    	self.perimeter = round(math.pi * self.diameter, 3)
